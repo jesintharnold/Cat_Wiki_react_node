ef0b9d9f1d5f7f99f86550a7f05e2820e03576b4
Craxy_Jesi <39983328+jesintharnold@users.noreply.github.com>
2021-08-08 22:57:00 +0530

    Modal autoclose on click

--- front_end_react/src/App.js
+++ front_end_react/src/App.js
@@ -104,9 +104,7 @@ function App() {
          <img src={Image_1} alt="Not found"/>
          <img src={Image_2} alt="Not found"/>
          <img src={Image_3} alt="Not found"/>
-      
-       
-       </div>
+      </div>
       </div>
       </section>
 
--- front_end_react/src/Components/SearchModal.js
+++ front_end_react/src/Components/SearchModal.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactDom from "react-dom";
-
-
-import "./SearchModal.scss";
+import data from '../cat.json';
+import "../Styles/SearchModal.scss";
 const SearchModal=({modal,setModal})=>{
+   
+    const [suggest,setSuggest]=useState([]);
+    const [text,setText]=useState("");
+    const input_ref=useRef();
+
+
+
+    
+
+    const onchange=(val)=>{
+        
+       if(val!==(" "&&"")){
+        const func_dat=data["cat"].filter(({first_name})=>{
+            return first_name.toLocaleLowerCase().includes(val.toLocaleLowerCase())
+        });
+
+        setSuggest(func_dat);
+       }else{
+           setSuggest([]);
+       }
+    };
+
+    const valChange=(textval)=>{
+           console.log(textval);
+           setText(textval);
+           input_ref.current.value=textval;
+           setSuggest([]);
+           
+    };
+
     return ReactDom.createPortal (
     <div className="modal_wrapper">
     <div className="modal-container">
     <div className="close_modal"  onClick={()=>setModal(!modal)}><i class="uil uil-times"></i></div>    
     <div className={`modal-search  search-container`}>
-          <input type="text" placeholder="Search"/>
+          <input type="text" placeholder="Search" ref={input_ref}  onChange={(e)=>onchange(e.target.value)}/>
           <button class="search__button">
           <i class="uil uil-search"></i>
           </button>
     </div>
-    <div className="recommends">
-    
+    <div className="recommends" >
+    {(suggest && suggest.length>0)?(
+        suggest.map(({first_name},index)=><div key={index} onClick={()=>valChange(first_name)}>{first_name}</div>)
+    ):null}
 
       
     </div>
